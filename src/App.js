42e38edb3b8d8486fe78997ca4eb68f2db6a4f3a
Daiana Gimenez <87146334+NamineD@users.noreply.github.com>
2022-02-16 17:22:08 -0300

    Uploading the project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import { gql, useQuery } from '@apollo/client'
+import ListMissions from './components/ListMissions';
+import Mission from './components/Mission';
+
+const launches_past = gql`
+query {
+    launchesPast(limit: 10) {
+        mission_name
+        launch_date_local
+        details
+        launch_site {
+          site_name_long
+        }
+        links {
+            article_link
+            flickr_images
+        }
+    }
+}
+`
 
 function App() {
+
+  const {data, error, loading} = useQuery(launches_past)
+
+  const [ selectedMission, setSelectedMission ] = useState(null)
+
+  if(error) return <span style='color: red'>{error}</span>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='container-app row '> 
+        <div  className="containerMissions card col-12 col-sm-6 col-xl-5 p-0 m-0">
+          <h1 className='m-3'>Last Launches</h1>
+          { loading 
+            ? <div class="d-flex justify-content-center">
+                <div class="spinner-border" role="status">
+                  <span class="visually-hidden">Loading...</span>
+                </div>
+              </div>
+            : 
+                <div className='listMissions'><ListMissions setSelectedMission={setSelectedMission}  mission={data?.launchesPast} /></div>            
+          }
+        </div>
+        <div className='MissionDetails col-12 col-sm-6 col-xl-7 p-0 m-0'><Mission selectedMission={selectedMission}/></div>
+      </div>
   );
 }
 
